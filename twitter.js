0f30462b8f9173304a8bc819466f34e42367b7dc
DanyalXD <danyal1995@hotmail.co.uk>
2016-04-17 01:46:28 +0100

    nice

---
+++ twitter.js
@@ -0,0 +1,28 @@
+var Twitter = require('twitter');
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+//you need to put your own keys in here
+var client = new Twitter({
+    consumer_key: 'W0qmXQ9ZcZ331lRRSOH92vfoo',
+    consumer_secret: '  ursoNPsoAPgBfLaAi4UvOqSoDO89fGAXRX2GrjIpOMeXWa4LPg',
+    access_token_key: ' 389398408-Rq0ejKRxZNx1ymPIUXZwBqeBrM5MVvbeZ6RQimVa',
+    access_token_secret: '  1yAmkT8CDOIr1WcklgDioEYFv5KII9ye506EeCzNFblTH'
+});
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*'  });
+    //search for 10 tweets containing lolcats
+    client.get('search/tweets', {q: 'lolcats' , count: '10'}, function(error, tweets){
+
+        var json = [];
+        for (var i =0; i< tweets.statuses.length ; i++)
+        {
+            json.push({name: tweets.statuses[i].user.name, text: tweets.statuses[i].text});
+        }
+
+        response.end(JSON.stringify(json));
+    });
+}).listen(port);
+
+
